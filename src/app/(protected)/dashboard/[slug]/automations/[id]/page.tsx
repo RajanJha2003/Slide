7c45f28a5f4508a8042351b73af608d64c7bd45d
Rajan <rajanj448@student.sfit.ac.in>
2024-12-19 18:46:03 +0530

    working

--- src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -1,4 +1,5 @@
 import AutomationsBreadCrumb from '@/components/global/bread-crumbs/automations'
+import { Warning } from '@/icons'
 import React from 'react'
 
 type Props={
@@ -11,6 +12,14 @@ const page = ({params}:Props) => {
   return (
     <div className='flex flex-col items-center gap-y-20'>
         <AutomationsBreadCrumb id={params.id} />
+        <div className='w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3'>
+
+            <div className='flex gap-x-2'>
+                <Warning />
+                When...
+
+            </div>
+        </div>
     </div>
   )
 }
