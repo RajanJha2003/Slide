2dcee9d2003e82092c7a3caf9a298367b519c46d
Rajan <rajanj448@student.sfit.ac.in>
2024-12-19 18:17:19 +0530

    automation list page

--- src/app/(protected)/dashboard/[slug]/automations/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/page.tsx
@@ -1,3 +1,4 @@
+import AutomationList from '@/components/global/automation-list'
 import CreateAutomation from '@/components/global/create-automation'
 import { Check } from 'lucide-react'
 import React from 'react'
@@ -6,7 +7,7 @@ const page = () => {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-6 gap-5'>
         <div className='lg:col-span-4'>
-            Automate List
+           <AutomationList />
 
 
         </div>
