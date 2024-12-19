9c783d0fddcafd811f2d9d497109c0f53c4aa066
Rajan <rajanj448@student.sfit.ac.in>
2024-12-19 10:02:36 +0530

    theme dark and light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from '@clerk/nextjs'
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -17,7 +18,16 @@ export default function RootLayout({
   return (
   <ClerkProvider>
       <html lang="en">
-      <body suppressHydrationWarning className={jakarta.className}>{children}</body>
+      <body suppressHydrationWarning className={jakarta.className}>
+        <ThemeProvider  attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange>
+        {children}
+
+        </ThemeProvider>
+        
+        
+        </body>
     </html>
   </ClerkProvider>
   );
