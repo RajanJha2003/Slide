4dcd0e50e8298861107dcdf27b54c13d9967a962
Rajan <rajanj448@student.sfit.ac.in>
2024-12-19 13:26:08 +0530

    constants done

--- src/components/global/infobar/index.tsx
+++ src/components/global/infobar/index.tsx
@@ -1,6 +1,17 @@
+"use client";
+
+import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
+import { usePaths } from '@/hooks/user-nav'
 import React from 'react'
 
-const InfoBar = () => {
+type Props={
+    slug:string
+}
+
+const InfoBar = ({slug}:Props) => {
+
+    const { page } = usePaths()
+    const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
   return (
     <div>InfoBar</div>
   )
