6ba91bc6374c56360d2008af4bdc1fd8fa7914d3
Rajan <rajanj448@student.sfit.ac.in>
2024-12-19 13:39:36 +0530

    navabr completed

--- src/components/global/infobar/index.tsx
+++ src/components/global/infobar/index.tsx
@@ -1,33 +1,36 @@
-"use client";
-
+'use client'
 
 import { PAGE_BREAD_CRUMBS } from '@/constants/pages'
 import { usePaths } from '@/hooks/user-nav'
+import { Menu } from 'lucide-react'
 import React from 'react'
-import Sheet from '../sheet';
-import { Menu } from 'lucide-react';
-import { LogoSmall } from '@/svgs/logo-small';
-import Items from '../sidebar/items';
-import { Separator } from '@/components/ui/separator';
-import ClerkAuthState from '../clerk-auth-state';
-import { HelpDuoToneWhite } from '@/icons';
-import { SubscriptionPlan } from '../subscription-plan';
-import UpgradeCard from '../sidebar/upgrade';
+import Sheet from '../sheet'
+import Items from '../sidebar/items'
+import { Separator } from '@/components/ui/separator'
+import ClerkAuthState from '../clerk-auth-state'
+import { HelpDuoToneWhite } from '@/icons'
+import { SubscriptionPlan } from '../subscription-plan'
+import UpgradeCard from '../sidebar/upgrade'
+import { LogoSmall } from '@/svgs/logo-small'
+// import CreateAutomation from '../create-automation'
+import Search from './search'
+import { Notifications } from './notifications'
+import MainBreadCrumb from '../bread-crumbs/main-bread-crumb'
 
-type Props={
-    slug:string
+type Props = {
+  slug: string
 }
 
-const InfoBar = ({slug}:Props) => {
+const InfoBar = ({ slug }: Props) => {
+  const { page } = usePaths()
+  const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
 
-    const { page } = usePaths()
-    const currentPage = PAGE_BREAD_CRUMBS.includes(page) || page == slug
   return (
     currentPage && (
-        <div className='flex flex-col'>
-            <div className="flex gap-x-3 lg:gap-x-5 justify-end">
-                <span className='lg:hidden flex items-center flex-1 gap-x-2'>
-                <Sheet
+      <div className="flex flex-col">
+        <div className="flex gap-x-3 lg:gap-x-5 justify-end">
+          <span className="lg:hidden flex items-center flex-1 gap-x-2">
+            <Sheet
               trigger={<Menu />}
               className="lg:hidden"
               side="left"
@@ -65,11 +68,16 @@ const InfoBar = ({slug}:Props) => {
                 </SubscriptionPlan>
               </div>
             </Sheet>
-                   
-                </span>
-            </div>
-
+          </span>
+          <Search />
+          {/* <CreateAutomation /> */}
+          <Notifications />
         </div>
+        <MainBreadCrumb
+          page={page === slug ? 'Home' : page}
+          slug={slug}
+        />
+      </div>
     )
   )
 }
