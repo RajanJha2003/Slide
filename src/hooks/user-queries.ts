43f1fc38d4511078a695f66aa0a60567325bc901
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 11:42:10 +0530

    billing

--- src/hooks/user-queries.ts
+++ src/hooks/user-queries.ts
@@ -1,9 +1,18 @@
 import { getAllAutomations } from "@/actions/automations"
+import { onUserInfo } from "@/actions/user"
 import { useQuery } from "@tanstack/react-query"
 
 export const useQueryAutomations = () => {
     return useQuery({
       queryKey: ['user-automations'],
       queryFn: getAllAutomations,
+    })
+  }
+
+
+  export const useQueryUser=()=>{
+    return useQuery({
+        queryKey:['user-profile'],
+        queryFn:onUserInfo
     })
   }
