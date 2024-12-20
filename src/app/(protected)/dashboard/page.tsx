214ce1ccf1dfe50abfa0a2af1430eae821fda5d6
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 10:40:30 +0530

    dashboard redirect

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import { onBoardUser } from '@/actions/user'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const page = async() => {
+
+  const user=await onBoardUser();
+  if(user.status===200 || user.status===201){
+    return redirect(`dashboard/${user.data?.firstname}${user.data?.lastname}`)
+  }
+
+
+  return redirect('/sign-in')
+  
 }
 
 export default page
