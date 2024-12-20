41736c63fa0a61a281d9c012d1503e3128e06d41
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 10:54:32 +0530

    get automations

---
+++ src/actions/automations/index.ts
@@ -0,0 +1,16 @@
+
+"use server";
+
+import { onCurrentUser } from "../user";
+
+export const getAllAutomations=async()=>{
+
+    const user=await onCurrentUser();
+
+    try {
+        const automations=await getA
+    } catch (error) {
+        
+    }
+
+}
