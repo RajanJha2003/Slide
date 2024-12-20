80207da7bad2158d274106ad7f2a812d70b5ac9e
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 10:51:37 +0530

    user info

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -83,7 +83,24 @@ export const onBoardUser=async()=>{
 
 export const onUserInfo=async()=>{
 
-    
+    const user=await onCurrentUser();
+    try {
+        const profile=await findUser(user.id);
+        if(profile){
+            return{
+                status:200,
+                data:profile
+            }
+        }
+
+        return {
+            status:404
+        }
+    } catch (error) {
+
+        return{status:500}
+        
+    }
 
         
 }
