0fc4c826c05681dd5abf298e4e8707e14ceb61d6
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 10:17:33 +0530

    node trigger

--- src/components/global/automations/post/node.tsx
+++ src/components/global/automations/post/node.tsx
@@ -1,8 +1,42 @@
+import { Separator } from '@/components/ui/separator'
+import { InstagramBlue, Warning } from '@/icons'
+import Image from 'next/image'
 import React from 'react'
 
-const PostNode = () => {
+type Props={
+    id:string
+}
+
+const PostNode = ({id}:Props) => {
   return (
-    <div>PostNode</div>
+    <div className="w-10/12 lg:w-8/12 relative xl:w-4/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3">
+        <div className="absolute h-20 left-1/2 bottom-full flex flex-col items-center z-50">
+          <span className="h-[9px] w-[9px] bg-connector/10 rounded-full" />
+          <Separator
+            orientation="vertical"
+            className="bottom-full flex-1 border-[1px] border-connector/10"
+          />
+          <span className="h-[9px] w-[9px] bg-connector/10 rounded-full" />
+        </div>
+        <div className="flex gap-x-2">
+          <Warning />
+          If they comment on...
+        </div>
+        <div className='bg-background-50 p-3 rounded-xl flex flex-col gap-y-2'>
+
+            <div className='flex gap-x-2 items-center'>
+                <InstagramBlue />
+                <p className='font-bold text-lg'>These posts</p>
+            </div>
+            <div className='flex gap-x-2 flex-wrap mt-3'>
+                <div className='relative w-4/12 aspect-square rounded-lg cursor-pointer overflow-hidden'>
+                <Image fill sizes='100vw' alt='post image' src={"/Ig-creators.png"} />
+
+                </div>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
