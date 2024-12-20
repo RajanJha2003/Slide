8b0a838f1305ec7ff272a4bfefb270c0bc4d73a8
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 10:56:41 +0530

    automation info and find

--- src/actions/automations/index.ts
+++ src/actions/automations/index.ts
@@ -2,15 +2,40 @@
 "use server";
 
 import { onCurrentUser } from "../user";
+import { findAutomation, getAutomations } from "./queries";
 
 export const getAllAutomations=async()=>{
 
     const user=await onCurrentUser();
 
     try {
-        const automations=await getA
+        const automations=await getAutomations(user.id);
+        if(automations){
+            return {
+                status:200,
+                data:automations.automations
+            }
+        }
+
+        return{
+            status:404,data:[]
+        }
     } catch (error) {
+        return { status: 500, data: [] }
         
     }
 
 }
+
+
+export const getAutomationInfo = async (id: string) => {
+    await onCurrentUser()
+    try {
+      const automation = await findAutomation(id)
+      if (automation) return { status: 200, data: automation }
+  
+      return { status: 404 }
+    } catch (error) {
+      return { status: 500 }
+    }
+  }
