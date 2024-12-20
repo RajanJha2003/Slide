54f3f39d40459343cfb7735ace7c45a5e2737103
Rajan <rajanj448@student.sfit.ac.in>
2024-12-20 11:32:10 +0530

    redux and react query provider

--- src/app/(protected)/dashboard/[slug]/_components/metrics/metrics-card.tsx
+++ src/app/(protected)/dashboard/[slug]/_components/metrics/metrics-card.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useQueryAutomations } from '@/hooks/user-queries';
 import React from 'react'
 
 const MetricsCard = () => {
+
+    const {data}=useQueryAutomations();
+
+    const comments=data?.data.reduce((current,next)=>{
+        return current+next.listener?.dmCount!
+    },0)
   return (
     <div>MetricsCard</div>
   )
